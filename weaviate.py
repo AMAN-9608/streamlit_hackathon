61f5f0be0bcb0589ad9747a720bff8dc967e69fe
pb2640 <pb2640@nyu.edu>
2023-09-18 23:13:56 -0400

    Delete weaviate.py

--- weaviate.py
+++
@@ -1,7 +0,0 @@
-import weaviate
-
-# Instantiate the client with the auth config
-client = weaviate.Client(
-    url="https://travel-pb-cluster-mfso2mhk.weaviate.network",  # Replace w/ your endpoint
-    auth_client_secret=weaviate.AuthApiKey(api_key="jVRRFvwW7iwiHhhOSbpUOnM2tPYYpaGHOy11"),  # Replace w/ your Weaviate instance API key
-)
